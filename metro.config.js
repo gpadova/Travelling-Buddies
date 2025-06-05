3a941631953c1e0933242062f1e76c7fca189edc
Giovanni Padova <gpadova@belmare.com.br>
2025-06-04 22:00:17 -0300

    gitignore fix

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,15 @@
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 const { addLiveStoreDevtoolsMiddleware } = require("@livestore/devtools-expo");
 
-const config = getDefaultConfig(__dirname);
 addLiveStoreDevtoolsMiddleware(config, {
   schemaPath: "./livestore/schema.ts",
 });
 
 config.resolver.unstable_enablePackageExports = false;
 module.exports = withNativeWind(config, { input: "./global.css" });
+
+module.exports = config;
